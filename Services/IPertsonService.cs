2a077b9d2390344c502d45e00af69cee4b811ec4
quoclong97 <quoclong3005@gmail.com>
2022-02-19 16:50:22 +0700

    add code day6

---
+++ Services/IPertsonService.cs
@@ -0,0 +1,11 @@
+namespace D6.Models;
+
+public interface IPersonService
+{
+    // List<Person> GetAll();
+    
+    // void Create(Person person);
+    // void Update(Person person);
+    // void Delete(int index);
+
+}
